3131aaf68e17d65c514d9bf64e78235438bab754
Brandon B <broe5392@yahoo.com>
2018-10-21 16:33:56 -0400

    made basic login page

---
+++ client/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const LoginForm = props =>
+    <fieldset className="fieldset">
+        <legend>Sign In or Sign Up</legend>
+        <h1 className="loginTitle">Perfect Body</h1>
+        <form>
+            <div className="grid-container">
+                <div className="grid-x">
+                    <label className="cell">
+                        Username:
+                        <input type="text" placeholder="PerfectBody@PerfectBody.com" name="username" />
+                    </label>
+                    <p className="help-text">Enter your email as your Username to sign in</p>
+                    <label className="cell">
+                        Password:
+                        <input type="password" placeholder="PerfectBody24@" name="password" />
+                    </label>
+                    <p className="help-text">Enter your Password here. (Must be at least 8 charecters)</p>
+                    <div className="cell large-offset-3">
+                        <button className="button success signUp">Sign Up</button>
+                        <button className="button success logIn">Log In</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </fieldset>
+
+export default LoginForm;
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
-import "./Login.css";
 //import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/Trunk";
-
+import LoginForm from "../../components/LoginForm";
 import API from "../../utils/API";
 
 class Login extends Component {
+
+  state = {
+    username: "",
+    password: ""
+  };
+
+
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <h1 className="login-center">
-            Login
-          </h1>
-        </Row>
-        <Row>
-          <button className="btn btn-outline-primary login-center" id="google-button" onClick={API.login}>
-            Google
-          </button>
-        </Row>
-      </Container>
+      <div className="grid-container">
+        <div className="grid-x loginGrid">
+          <div className="cell small-offset-3 loginCell">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
     );
   }
 }

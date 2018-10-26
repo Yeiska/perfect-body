c95c214eb6469cf8c8b5c701129d3b56089d754f
Brandon B <broe5392@yahoo.com>
2018-10-25 21:52:11 -0400

    Login header

---
+++ client/src/components/Header/HeaderLogin.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const HeaderLogin = props => 
+    // <div className="grid-x header">
+    //     <div className="large-12 cell">
+            <img className="header Login" src="http://breakthroughfitnesshhi.com/wp-content/uploads/Breakthrough-Fitness-Gym-Full-Body-Workout.png" alt="">
+                {props.children}
+            </img>
+        {/* </div>
+    </div> */}
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,2 +1,3 @@
 export * from "./Header";
-export * from "./HeaderNutrition"
+export * from "./HeaderNutrition";
+export * from "./HeaderLogin"
